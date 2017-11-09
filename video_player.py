91aaeaab9122a6f264afb75009cac62d0dc002ca
Comeme <developer.k.mshr0316@gmail.com>
2017-11-09 13:47:24 +0900

    一旦完成

--- video_player.py
+++ video_player.py
@@ -1,22 +1,51 @@
 # -*- coding: utf-8 -*-
 import cv2
 
+video_play_state = True
+
+
 class VideoPlayer(object):
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.source = filename
-        self.state = True
         self.cap = cv2.VideoCapture(filename)
+        self.frame_count = int(self.cap.get(7))
+        self.frame_rate = int(self.cap.get(5))
+        self.count = 0
+        self.is_end = False
+        cv2.namedWindow(self.source, cv2.WINDOW_AUTOSIZE)
+        # print(self.frame_count)
+        # print(self.frame_rate)
 
-
-    def play(self):
-        if self.state:
-            ret, frame = self.cap.read()
-            cv2.imshow("aaa", frame)
-
-
-    def check_state(self, new_state):
-        self.state = new_state
+    def play(self, state):
+        key = -1
+        if state:
+            if self.count >= self.frame_count:
+                self.is_end = True
+                return
+            is_read, frame = self.cap.read()
+            self.count += 1
+            if is_read:
+                key = cv2.waitKey(self.frame_rate)
+                if key == 27:
+                    self.is_end = True
+                else:
+                    cv2.imshow(self.source, frame)
+            else:
+                self.is_end = True
 
     def stop(self):
+        cv2.destroyAllWindows()
         self.cap.release()
 
+
+def main():
+    global video_play_state
+    player = VideoPlayer("tanioka.mp4")
+    while True:
+        player.play(video_play_state)
+        if player.is_end:
+            player.stop()
+            break
+
+if __name__ == '__main__':
+    main()
--- whole.py
+++ whole.py
@@ -4,8 +4,9 @@ import threading
 import cv2
 import sys
 
-global video_play_state
-video_play_state = 0
+video_play_state = True
+end_flag = False
+
 
 class IsLookingThread(threading.Thread):
     def __init__(self):
@@ -16,43 +17,72 @@ class IsLookingThread(threading.Thread):
         self.announcement = ["no face", "looking", "not looking"]
 
     def run(self):
-        while (self.cap.isOpened()):
-            ret, image = self.cap.read()
+        global video_play_state
+        global end_flag
+        while not end_flag:
+            is_read, image = self.cap.read()
             image = cv2.resize(image, (426, 240))
             result = self.estimater.estimate(image)
             self.state = result[0]
             masked_image = result[1]
 
             # cv2.imshow("angle", masked_image)
-            # sys.stdout.write("\r%s" % self.announcement[self.state])
-            video_play_state = self.state
+            sys.stdout.write("\r%s" % self.announcement[self.state])
+            if self.state == 1:
+                video_play_state = True
+            elif self.state == 2:
+                video_play_state = False
             # if cv2.waitKey(1) & 0xFF == ord('q'):
             #     break
 
+
 class VideoPlayer(object):
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.source = filename
         self.cap = cv2.VideoCapture(filename)
+        self.frame_count = int(self.cap.get(7))
+        self.frame_rate = int(self.cap.get(5))
+        self.count = 0
+        self.is_end = False
+        cv2.namedWindow(self.source, cv2.WINDOW_AUTOSIZE)
 
-
-    def play(self):
-        if video_play_state:
-            ret, frame = self.cap.read()
-            print(frame)
-            cv2.imshow("tanioka", frame)
+    def play(self, state):
+        key = -1
+        if state:
+            if self.count >= self.frame_count:
+                self.is_end = True
+                return
+            is_read, frame = self.cap.read()
+            self.count += 1
+            if is_read:
+                # key = cv2.waitKey(self.frame_rate)
+                key = cv2.waitKey(1)
+                if key == 27:
+                    self.is_end = True
+                else:
+                    cv2.imshow(self.source, frame)
+            else:
+                self.is_end = True
 
     def stop(self):
+        cv2.destroyAllWindows()
         self.cap.release()
 
 def main():
+    global video_play_state
+    global end_flag
     is_looking_thread = IsLookingThread()
-    video_player = VideoPlayer("tanioka.mp4")
+    player = VideoPlayer("tanioka.mp4")
     is_looking_thread.start()
-    while(True):
-        video_player.play()
+    while True:
+        # player.play(video_play_state)
+        player.play(video_play_state)
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            player.is_end = True
+        if player.is_end:
+            end_flag = True
+            player.stop()
             break
-    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
