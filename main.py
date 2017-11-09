4166e6a04a44f6f8870e48e4d67efa17bab1914c
Comeme <developer.k.mshr0316@gmail.com>
2017-11-09 18:26:46 +0900

    完成

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from estimete import Estimater
-import threading
 import cv2
 import sys
 import os
@@ -15,6 +14,7 @@ class FaceChecker(object):
         self.cap = cv2.VideoCapture(0)
         self.state = 0
         self.announcement = ["no face", "looking", "not looking"]
+        self.state_list = [0.5 for i in range(5)]
         cv2.namedWindow("capture", cv2.WINDOW_AUTOSIZE)
 
     def check(self):
@@ -31,9 +31,13 @@ class FaceChecker(object):
 
     def renew_state(self):
         global video_play_state
-        if self.state == 1:
+        if self.state != 0 :
+            for i in range(len(self.state_list) - 1):
+                self.state_list[i] = self.state_list[i + 1]
+                self.state_list[-1] = self.state
+        if 1.0 * sum(self.state_list)/len(self.state_list) < 1.5:
             video_play_state = True
-        elif self.state == 2:
+        else:
             video_play_state = False
 
     def quit(self):
@@ -44,7 +48,8 @@ class FaceChecker(object):
 class VideoPlayer(object):
     def __init__(self, filename):
         self.source = filename
-        self.apple_script = {'open': 'open.scpt',
+        self.apple_script = {'activate': 'activate.scpt',
+                             'open': 'open.scpt',
                              'play': 'play.scpt',
                              'pause': 'pause.scpt',
                              'quit': 'quit.scpt'}
@@ -56,36 +61,42 @@ class VideoPlayer(object):
         3...一時停止
         4...終了
         """
+        self.current_dir = os.getcwd()
+
+    def activate(self):
+        print("osascript %s" % self.current_dir + '/apple_scripts/' + self.apple_script['activate'])
+        os.system("osascript %s" % './apple_scripts/' + self.apple_script['activate'])
+        self.state = 0
 
     def open(self):
-        os.system("osascript %s" % self.apple_script['open'])
+        os.system("osascript %s %s" % (self.current_dir + '/apple_scripts/'+self.apple_script['open'], self.current_dir + '/videos/' + self.source))
         self.state = 1
 
     def play(self):
-        os.system("osascript %s" % self.apple_script['play'])
+        os.system("osascript %s" % self.current_dir + '/apple_scripts/' + self.apple_script['play'])
         self.state = 2
 
     def pause(self):
-        os.system("osascript %s" % self.apple_script['pause'])
+        os.system("osascript %s" % self.current_dir + '/apple_scripts/' + self.apple_script['pause'])
         self.state = 3
 
     def quit(self):
-        os.system("osascript %s" % self.apple_script['quit'])
+        os.system("osascript %s" % self.current_dir + '/apple_scripts/' + self.apple_script['quit'])
         self.state = 4
 
 
-def main():
+def main(video_file):
     global video_play_state
     global end_flag
 
     face_checker = FaceChecker()
-    player = VideoPlayer("sun-chang.mp4")
+    player = VideoPlayer(video_file)
+    player.activate()
     player.open()
     while not end_flag:
         face_checker.check()
         face_checker.renew_state()
         sys.stdout.write("\rplayer state: %d" % player.state)
-        # print(player.state)
         if player.state in (1, 3) and video_play_state:
             player.play()
         if player.state == 2 and not video_play_state:
@@ -93,8 +104,9 @@ def main():
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             end_flag = True
+            player.state = 4
     player.quit()
     face_checker.quit()
 
 if __name__ == '__main__':
-    main()
+    main('tanioka.mp4')
